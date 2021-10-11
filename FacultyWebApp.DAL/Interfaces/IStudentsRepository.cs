d9954d49c60f5ff17bfe2f69cc4753caf1ecc9c9
mrgumorist <mrgumorist@gmail.com>
2021-10-11 18:12:38 +0300

    Created user repository

---
+++ FacultyWebApp.DAL/Interfaces/IStudentsRepository.cs
@@ -0,0 +1,12 @@
+﻿using FacultyWebApp.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FacultyWebApp.DAL.Interfaces
+{
+    public interface IStudentsRepository:IGenericRepository<Student>
+    {
+        IEnumerable<Student> GetStudentsByFilters(string Surname, bool? IsDeducted, int? groupId);
+    }
+}
---
+++ FacultyWebApp.DAL/Repositories/StudentsRepository.cs
@@ -0,0 +1,22 @@
+﻿using FacultyWebApp.DAL.Entities;
+using FacultyWebApp.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace FacultyWebApp.DAL.Repositories
+{
+    public class StudentsRepository:GenericRepository<Student>, IStudentsRepository
+    {
+        public StudentsRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public IEnumerable<Student> GetStudentsByFilters(string Surname, bool? IsDeducted, int? groupId)
+        {
+            //TODO: filtering
+            return _context.Students.Take(5);
+        }
+    }
+}
